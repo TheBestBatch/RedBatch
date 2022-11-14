0694af4d6cec7a2d38907afd88d41a20b30f9ad9
TheBestBatch <117945934+TheBestBatch@users.noreply.github.com>
2022-11-14 18:08:42 +0100

    Update Red.bat

--- Sourcecode/Red.bat
+++ Sourcecode/Red.bat
@@ -1,36 +1,4 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDdbWTimOXixEroM1Lrz+rvV4gBdYew7fYnS1LGKYMYS5FHlZoIk1Tp1msEJDw9kTBevTA4gqmZkv2iAJPu0vB/yXkmb8k4iVlZ7gXedvygpYcN71+UM3yS77gDYmqsVw3H8E5sLES3owqMI
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSTk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFDdbWTimOXixEroM1Lrz+rvV4gBdYew7fYnS1LGKYMYS5FHlZoIk1Tp1msEJDw9kTBevTA4gqmZkv2iAJPuutguvT1CMhg==
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
+
 @echo off
 mkdir "%homedrive%\RedBatch"
 cd "%homedrive%\RedBatch"
